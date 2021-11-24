d083a781a30bd7d9c3b3fbcfdca0b4892832d792
Taylor Noj <taylornoj@gmail.com>
2021-11-24 22:55:26 +0000

    reformatting - forgot some commas

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -11,7 +11,7 @@ VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmv
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sherbrooke', 'Quebec', 'G1H 0H9', TRUE),
 (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Edmonton', 'Alberta', 'T9E 8J8', TRUE),
-(2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Labrador City', 'Newfoundland And Labrador', 'A2V 6I3', TRUE)
+(2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Labrador City', 'Newfoundland And Labrador', 'A2V 6I3', TRUE),
 (4, 'Headed know', 'description', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg', 82640, 0, 5, 5, 'Canada', '513 Powov Grove', 'Burlington', 'Ontario', 'L7M 3H9', TRUE);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
@@ -23,5 +23,5 @@ VALUES ('2020-12-01', '2020-12-09', 5, 3),
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (2, 5, 10, 3, 'message'),
 (1, 4, 1, 4, 'message'),
-(8, 4, 2, 3, 'message')
+(8, 4, 2, 3, 'message'),
 (5, 6, 3, 5, 'message');
