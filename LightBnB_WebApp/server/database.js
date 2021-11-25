78a57c1650da737b92693908c9cf9e384c48e141
Taylor Noj <taylornoj@gmail.com>
2021-11-25 22:56:34 +0000

    refine filters in search

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -75,8 +75,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   LIMIT $2`, [guest_id, limit])
   .then((result) => result.rows) 
   .catch((err) => console.log(err.message)); 
-  
-  //return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -91,12 +89,43 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-    .query (`SELECT * FROM properties LIMIT $1`, [limit])
+    const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      queryString += `AND properties.owner_id LIKE $${queryParams.length} `;
+    }
+
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+      queryString += `AND (properties.cost_per_night >= $${queryParams.length - 1} AND properties.cost_per_night <= $${queryParams.length})`;
+    }
+
+   if (options.minimum_rating) {
+     queryParams.push(options.minimum_rating);
+     queryString += `AND property_reviews.rating >= $${queryParams.length}`;
+   }
+  
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    return pool.query(queryString, queryParams)
     .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .catch((err) => console.log(err.message));
   };
 exports.getAllProperties = getAllProperties;
 
